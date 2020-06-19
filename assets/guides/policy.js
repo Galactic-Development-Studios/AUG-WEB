8e99f7c720f1085d7866bfff922b96af3d13c12d
Sam Neale <sammneale@icloud.com>
2020-06-19 12:56:00 +1000

    made progress on policies.

---
+++ assets/guides/policy.js
@@ -0,0 +1,57 @@
+var policyBody = document.getElementById("bodyPolicy");
+	var header1 = document.createElement("H1");
+	var para1 = document.createElement("P");
+	header1.innerHTML = "Welcome to Aussie Games' Policy Page";
+	para1.innerHTML = "Here you can find out information on how we process your data, termination, you rights and many more by clicking a page to the left. We must warn you that by visiting another page on the site or interacting with our community clasifies as accepting our TAC and other related polocies";
+	policyBody.appendChild(header1);
+	policyBody.appendChild(para1);
+function changePolicyWelcome(){
+	policyBody.innerHTML = "";
+	var header1 = document.createElement("H1");
+	var para1 = document.createElement("P");
+	header1.innerHTML = "Welcome to Aussie Games' Policy Page";
+	para1.innerHTML = "Here you can find out information on how we process your data, termination, you rights and many more by clicking a page to the left. We must warn you that by visiting another page on the site or interacting with our community clasifies as accepting our TAC and other related polocies";
+	policyBody.appendChild(header1);
+	policyBody.appendChild(para1);
+}
+function changePolicyTerminology(){
+	policyBody.innerHTML = "";
+	var headerMain = document.createElement("H1");
+	var header1 = document.createElement("H2");
+	var para1 = document.createElement("P");
+	headerMain.innerHTML = "Who is who here?";
+	para1.innerHTML = "The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: 'Client', 'You' and 'Your' refers to you, the person log on this website and compliant to the Company's terms and conditions. 'The Company', 'Ourselves', 'We', 'Our', 'AUG' and 'Us', refers to our Company. 'Party', 'Parties', or 'Us', refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client's needs in respect of provision of the Company's stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.";
+	policyBody.appendChild(headerMain);
+	policyBody.appendChild(para1);
+}
+function changePolicyCookies(){
+	policyBody.innerHTML = "";
+	var headerMain = document.createElement("H1");
+	var header1 = document.createElement("H2");
+	var para1 = document.createElement("P");
+	headerMain.innerHTML = "Cookies";
+	para1.innerHTML = "We employ the use of cookies. By accessing AUG, you agreed to use cookies in agreement with the Aussie Games Privacy Policy. Most interactive websites use cookies to let us retrieve the user's details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.";
+	policyBody.appendChild(headerMain);
+	policyBody.appendChild(para1);
+}
+
+function changePolicyTermination(){
+	policyBody.innerHTML = "";
+	var headerMain = document.createElement("H1");
+	var header1 = document.createElement("H2");
+	var para1 = document.createElement("P");
+	headerMain.innerHTML = "Termination";
+	para1.innerHTML = "AUG reserves the right to terminate your membership at any point in time, for any reason or no reason at all. However we may terminate your membership for any of the following.<br><br>Inactivity<br>Abusive Behaviour (Swearing, Bullying, ect.)";
+	policyBody.appendChild(headerMain);
+	policyBody.appendChild(para1);
+}
+function changePolicyRights(){
+	policyBody.innerHTML = "";
+	var headerMain = document.createElement("H1");
+	var header1 = document.createElement("H2");
+	var para1 = document.createElement("P");
+	headerMain.innerHTML = "Rights";
+	para1.innerHTML = "AUG reserves the right to terminate your membership at any point in time, for any reason or no reason at all. However we may terminate your membership for any of the following.<br><br>Inactivity<br>Abusive Behaviour (Swearing, Bullying, ect.)";
+	policyBody.appendChild(headerMain);
+	policyBody.appendChild(para1);
+}
