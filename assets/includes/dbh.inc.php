65b50b53784180093e3f440bf5673161265c188e
Sam-Neale <sammneale@icloud.com>
2020-06-28 22:14:15 +1000

    revert to V1 AUG

--- assets/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "usercenterlogin";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
--- script2.js
+++
@@ -1,6 +0,0 @@
-function hideSupportedGames(){
-	document.getElementById("supportedGames").style.display = "none";
-}
-function showSupportedGames(){
-	document.getElementById("supportedGames").style.display = "block";
-}
