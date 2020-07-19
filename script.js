8ed56feba58468a67b4fcd61276af3b28d3f857f
Sam-Neale <sammneale@icloud.com>
2020-07-19 13:17:25 +1000

    added icons and navbar

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-//Game List
-function showGameList(){
-	document.getElementById('gameList').style.display= 'block';
-	document.getElementById('close1').style.display = 'none';
-		document.getElementById('close2').style.display = 'none';
-			document.getElementById('close3').style.display = 'none';
-	document.getElementById('gameList').innerHTML = "<div id='gameListItemFullWidth'><p>Click window to close</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/FL.webp'><p>Flashing Lights</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/GM.webp'><p>Garry's Mod</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/IF.webp'><p>Infinite Flight</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/KSP.webp'><p>Kerbal Space Program</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/MC.webp'><p>Minecraft</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/RB.webp'><p>Roblox</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/SM.webp'><p>Surving Mars</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/re.webp'><p>Reentry</p></div>"
+var state = "hidden";
+function toggleMobNavBar(){
+	
+	var navbar = document.getElementById("mobileNavBar");
+	if (state == "hidden"){
+		navbar.style.left = "0%";
+		state = "shown";
+		console.log("openMobNav");
+	}else if (state == "shown"){
+		navbar.style.left = "-30%";
+		state = "hidden";
+		console.log("closingMobNav");
 }
-function closeGameList(){
-		document.getElementById('gameList').style.display= 'none';
-		document.getElementById('close1').style.display = 'block';
-		document.getElementById('close2').style.display = 'block';
-			document.getElementById('close3').style.display = 'block';
-			document.getElementById('gameList').innerHTML = '';
-}
-function timeOutCheck(){
-	loadVar = setTimeout(loadFin, 2500);
-}
-function loadFin(){
-	var page = document.getElementById('page');
-	var spinner = document.getElementById('loader');
-
-	spinner.style.display='none';
-	page.style.display='block';
 }
