a0c1560c9bebb6b12c888d8a47de2aee94cb43c8
Sam-Neale <sammneale@icloud.com>
2020-08-10 18:33:04 +1000

    iksa inspired

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-//Game List
-function showGameList(){
-	document.getElementById('gameList').style.display= 'block';
-	document.getElementById('close1').style.display = 'none';
-		document.getElementById('close2').style.display = 'none';
-			document.getElementById('close3').style.display = 'none';
-	document.getElementById('gameList').innerHTML = "<div id='gameListItemFullWidth'><p>Click window to close</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/FL.webp'><p>Flashing Lights</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/GM.webp'><p>Garry's Mod</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/IF.webp'><p>Infinite Flight</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/KSP.webp'><p>Kerbal Space Program</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/MC.webp'><p>Minecraft</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/RB.webp'><p>Roblox</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/SM.webp'><p>Surving Mars</p></div><div id='gameListItem'><img id='gameListItemImg' src='assets/images/gameIcons/re.webp'><p>Reentry</p></div>"
+function openMobNav(){
+	var MobNav = document.getElementById("mobNav");
+	var MobNavButton = document.getElementById("mobNavButton");
+	document.body.style.overflowY = "hidden";
+	document.getElementById("noScroll").style.overflowY = "hidden";
+	mobNav.style.right = "0%";
+	mobNavButton.style.display = "none";
+	window.scrollTo(0,0);
 }
-function closeGameList(){
-		document.getElementById('gameList').style.display= 'none';
-		document.getElementById('close1').style.display = 'block';
-		document.getElementById('close2').style.display = 'block';
-			document.getElementById('close3').style.display = 'block';
-			document.getElementById('gameList').innerHTML = '';
-}
-function timeOutCheck(){
-	loadVar = setTimeout(loadFin, 2500);
-}
-function loadFin(){
-	var page = document.getElementById('page');
-	var spinner = document.getElementById('loader');
-
-	spinner.style.display='none';
-	page.style.display='block';
+function closeMobNav(){
+	var MobNav = document.getElementById("mobNav");
+	var MobNavButton = document.getElementById("mobNavButton");
+	document.body.style.overflowY = "auto";
+	document.getElementById("noScroll").style.overflowY = "auto";
+	mobNav.style.right = "-110%";
+	mobNavButton.style.display = "block";
+	window.scrollTo(0,0);
 }
